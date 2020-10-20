3a415bf10ad5c7fdd94cb0ee0961accf9f8fd3e7
luca-dex <luca.desano@gmail.com>
2020-10-20 10:34:40 +0200

    update to 1.15.4

--- R/deprecated.CIMLR.R
+++ R/deprecated.CIMLR.R
@@ -17,16 +17,10 @@
 #' @return a list of 2 elements: K1 and K2 with an estimation of the best clusters (the lower   
 #' values the better) as discussed in the original paper of SIMLR
 #'
-
-
 "CIMLR_Estimate_Number_of_Clusters" = function(
     all_data,
     NUMC = 2:5,
     cores.ratio = 1) {
-    if (!requireNamespace("CIMLR", quietly = TRUE)) {
-        stop("Package \"CIMLR\" needed for this function to work. Please install it from https://github.com/danro9685/CIMLR",
-        call. = FALSE)
-    }
     .Defunct("CIMLR_Estimate_Number_of_Clusters", package = "CIMLR", 
         msg = "This function has been removed and it is now provided by the CIMLR package, please refer to https://github.com/danro9685/CIMLR")
 }
@@ -50,10 +44,6 @@
 #' @export CIMLR        
 #'  
 "CIMLR" <- function( X, c, no.dim = NA, k = 10, cores.ratio = 1 ) {
-    if (!requireNamespace("CIMLR", quietly = TRUE)) {
-        stop("Package \"CIMLR\" needed for this function to work. Please install it from https://github.com/danro9685/CIMLR",
-        call. = FALSE)
-    }
     .Defunct("CIMLR", package = "CIMLR", 
         msg = "This function has been removed and it is now provided by the CIMLR package, please refer to https://github.com/danro9685/CIMLR")
 }
